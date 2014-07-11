077e87425a3cc44acd0867e34446aed2131e75e9
goldenromeo <rommeltrejo@gmail.com>
2014-07-10 22:08:38 -0400

    summer

--- Hangan_game/src/game/Hangman.java
+++ Hangan_game/src/game/Hangman.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+* free and open to anybody. feel free to contribute
  */
 
 package game;
@@ -10,7 +8,7 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 
 /**
- *
+ *new edit
  * @author meep
  */
 public class Hangman {
